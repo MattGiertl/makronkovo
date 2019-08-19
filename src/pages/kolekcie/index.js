07aa37ee83caf02aa7a7486cb637fa3f395fe3a0
matt.giertl <matus1giertl@gmail.com>
2019-08-19 18:33:44 +0200

    removed unused import

--- src/pages/kolekcie/index.js
+++ src/pages/kolekcie/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import CollectionTemplate from '../../components/templates/CollectionTemplate';
 
 const CollectionPage = ({ pageContext }) => {
