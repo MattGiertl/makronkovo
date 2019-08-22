b98bb6568e9d8c9e90bbc79607c38fc8e9c9acee
Matúš Giertl <matus1giertl@gmail.com>
2019-08-22 16:17:23 +0200

    stylesheets added (#42)

--- src/components/atoms/Slideshow.js
+++ src/components/atoms/Slideshow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import '../../slideshow.css';
+import '../../stylesheets/slideshow.css';
 
 import Carousel from 'nuka-carousel';
 
