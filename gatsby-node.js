d58b2c84a54e9fbd528b0ca92d713899031d63c5
matt.giertl <matus1giertl@gmail.com>
2019-08-23 14:00:35 +0200

    generated category items template, data binding, mock data

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,20 +3,38 @@ const path = require('path');
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
 
-  const collectionTemplate = path.resolve(`src/templates/CollectionTemplate.js`);
+  const templatePath = 'src/templates';
+
+  const collectionTemplate = path.resolve(`${templatePath}/CollectionTemplate.js`);
+  const categoryItemTemplate = path.resolve(`${templatePath}/CategoryItemTemplate.js`);
 
   const result = await graphql(`
     {
       allCategoriesJson {
         edges {
           node {
+            boldText
+            lightText
             title
             to
-            lightText
-            boldText
             categoryItems {
               categoryName
               image
+              to
+            }
+          }
+        }
+      }
+      allCategoryItemsJson {
+        edges {
+          node {
+            title
+            to
+            items {
+              image
+              newPrice
+              oldPrice
+              title
               to
             }
           }
@@ -34,6 +52,14 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     createPage({
       path: node.to,
       component: collectionTemplate,
+      context: { ...node },
+    });
+  });
+
+  result.data.allCategoryItemsJson.edges.forEach(({ node }) => {
+    createPage({
+      path: node.to,
+      component: categoryItemTemplate,
       context: { ...node },
     });
   });
--- src/templates/CategoryItemTemplate.js
+++ src/templates/CategoryItemTemplate.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import Layout from '../components/organisms/Layout/Layout';
+import styled from '@emotion/styled';
+import Polaroid from '../components/molecules/Polaroid';
+import FountainHeading from '../components/molecules/FountainHeading';
+import Paragraph from '../components/atoms/Paragraph';
+import { mobileQuery, tabletQuery, laptopQuery, desktopQuery } from '../utils/mediaqueries';
 
-const CategoryItemTemplate = () => {
-  return <Layout>dopici</Layout>;
+const CategoriesWrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+
+  [mobileQuery]: {
+    margin: '0 35px',
+  },
+
+  [tabletQuery]: {
+    margin: '0 5%',
+  },
+
+  [laptopQuery]: {
+    margin: '0 15%',
+  },
+
+  [desktopQuery]: {
+    width: '1300px',
+    margin: '70px auto 0',
+  },
+});
+
+const CategoryItemTemplate = ({ pageContext }) => {
+  const { items, title } = pageContext;
+
+  const renderItems = items.map(item => (
+    <Polaroid shadowed src={item.image} >
+      <Paragraph>{item.title}</Paragraph>
+      {item.oldPrice && <Paragraph>{item.oldPrice}</Paragraph>}
+      <Paragraph>{item.newPrice}</Paragraph>
+    </Polaroid>
+  ));
+
+  return (
+    <Layout>
+      <FountainHeading>{title}</FountainHeading>
+      <CategoriesWrapper>
+        {renderItems}
+      </CategoriesWrapper>
+    </Layout>
+  );
 };
 
 export default CategoryItemTemplate;
