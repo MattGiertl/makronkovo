afc0d9c0ef47e1c1102bb121c84b25dd896db1c5
Lukáš Rzavský <lukasrzavsky@gmail.com>
2019-08-24 18:43:31 +0200

    gdpr link added

--- src/components/atoms/Anchor.js
+++ src/components/atoms/Anchor.js
@@ -1,41 +1,46 @@
-import React from "react";
-import styled from "@emotion/styled";
-import theme from "../../utils/theme/theme";
-import {
-  tabletQuery,
-  mobileQuery,
-  desktopQuery,
-  laptopQuery
-} from "../../utils/mediaqueries";
+import React from 'react';
+import styled from '@emotion/styled';
+import theme from '../../utils/theme/theme';
+import { tabletQuery, mobileQuery, desktopQuery, laptopQuery } from '../../utils/mediaqueries';
 
 const StyledAnchor = styled.a(
   {
     color: theme.colors.mediumGold,
-    textDecoration: "none",
-
-    [mobileQuery]: {
-      fontSize: theme.fontSizes.small
-    },
-
-    [tabletQuery]: {
-      fontSize: theme.fontSizes.medium
-    },
-
-    [laptopQuery]: {
-      fontSize: theme.fontSizes.medium
-    },
-
-    [desktopQuery]: {
-      fontSize: theme.fontSizes.medium
-    }
+    textDecoration: 'none',
   },
   props => ({
     fontFamily: props.fontFamily ? props.fontFamily : theme.fontFamilies.dinPro.regular,
-  })
+    [mobileQuery]: {
+      fontSize: props.mobileFontSize ? props.mobileFontSize : theme.fontSizes.small,
+      margin: props.mobileMargin,
+    },
+
+    [tabletQuery]: {
+      fontSize: props.tabletFontSize ? props.tabletFontSize : theme.fontSizes.medium,
+    },
+
+    [laptopQuery]: {
+      fontSize: props.laptopFontSize ? props.laptopFontSize : theme.fontSizes.medium,
+    },
+
+    [desktopQuery]: {
+      fontSize: props.desktopFontSize ? props.desktopFontSize : theme.fontSizes.medium,
+    },
+  }),
 );
 
-const Anchor = ({ href, children, fontFamily }) => (
-  <StyledAnchor href={href} fontFamily={fontFamily}>{children}</StyledAnchor>
+const Anchor = ({ href, children, fontFamily, mobileFontSize, tabletFontSize, laptopFontSize, desktopFontSize, mobileMargin }) => (
+  <StyledAnchor
+    href={href}
+    fontFamily={fontFamily}
+    mobileFontSize={mobileFontSize}
+    tabletFontSize={tabletFontSize}
+    laptopFontSize={laptopFontSize}
+    desktopFontSize={desktopFontSize}
+    mobileMargin={mobileMargin}
+  >
+    {children}
+  </StyledAnchor>
 );
 
 export default Anchor;
