56ef3c044c206288abc0cbebf0adbdb708921173
Lukáš Rzavský <lukasrzavsky@gmail.com>
2019-08-24 19:20:50 +0200

    prettier used

--- src/components/atoms/Drawing.js
+++ src/components/atoms/Drawing.js
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
 import { mobileQuery } from '../../utils/mediaqueries';
 
-
 const Drawing = styled.img({
-    [mobileQuery]: {width:"100%"}
-})
+  [mobileQuery]: { width: '100%' },
+});
 
 export default Drawing;
