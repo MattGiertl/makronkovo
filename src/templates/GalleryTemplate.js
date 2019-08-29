4625dd2946822645eb39537223b363a3c5a841b5
Lukáš Rzavský <lukasrzavsky@gmail.com>
2019-08-29 09:52:36 +0200

    images source fixed

--- src/templates/GalleryTemplate.js
+++ src/templates/GalleryTemplate.js
@@ -2,16 +2,13 @@ import React from 'react';
 import styled from '@emotion/styled';
 import Layout from '../components/organisms/Layout/Layout';
 import FountainHeading from '../components/molecules/FountainHeading';
-import { mobileQuery, tabletQuery, laptopQuery, desktopQuery } from '../utils/mediaqueries';
+import { laptopQuery, desktopQuery, mobileToTabletQuery } from '../utils/mediaqueries';
 
 const ContentWrapper = styled.div({
   display: 'flex',
   justifyContent: 'center',
   flexWrap: 'wrap',
-  [mobileQuery]: {
-    width: '100%',
-  },
-  [tabletQuery]: {
+  [mobileToTabletQuery]: {
     width: '100%',
   },
   [laptopQuery]: {
