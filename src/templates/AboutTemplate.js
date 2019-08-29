2ef1b4319d09fa199913519f0282f8f2d28fbbec
lukasrzavsky <36069536+lukasrzavsky@users.noreply.github.com>
2019-08-29 19:01:44 +0200

    About- Cards width fixed (#68)

--- src/templates/AboutTemplate.js
+++ src/templates/AboutTemplate.js
@@ -4,25 +4,27 @@ import Layout from '../components/organisms/Layout/Layout';
 import styled from '@emotion/styled';
 import { tabletQuery } from '../utils/mediaqueries';
 
-const ContentWrapper = styled.div(
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+const ContentWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  [tabletQuery]: {
+    marginBottom: '100px',
   },
-  props => ({
-    [tabletQuery]: {
-      marginBottom: '100px',
-    },
-  }),
-);
+});
+
+const CardsWrapper = styled.div({
+  width: '100%',
+});
 
 const AboutTemplate = ({ cards }) => {
   return (
     <Layout>
       <FountainHeading margin="0 5px 15px">O NÁS</FountainHeading>
-      <ContentWrapper>{cards}</ContentWrapper>
+      <ContentWrapper>
+        <CardsWrapper>{cards}</CardsWrapper>
+      </ContentWrapper>
     </Layout>
   );
 };
