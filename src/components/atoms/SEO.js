6a1d964707a9effb2b2de29372d0c34775eff193
matt.giertl <matus1giertl@gmail.com>
2019-09-01 12:28:08 +0200

    seo component added

---
+++ src/components/atoms/SEO.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+function SEO({ description, lang = 'sk', meta, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `,
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+      ].concat(meta)}
+    />
+  );
+}
+
+export default SEO;
