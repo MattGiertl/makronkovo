f96c9777ef04cdb60c38c64c341972c8768e4392
Matúš Giertl <matus1giertl@gmail.com>
2019-09-01 08:55:20 +0200

    Cms candybar (#78)
    * candybar cms config
    
    * config update
    
    * candybar folder added
    
    * removed testjson
    
    * test json added
    
    * name changed
    
    * renamed ot candybar data
    
    * folder collection path fix
    
    * config update #4
    
    * desssert update
    
    * unused space removed
    
    * Delete Kategórie - Candy bar “test”
    
    * label update
    
    * Create Kategórie - Candy bar “kusovky”
    
    * branch changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/src/data/candybar`,
+        name: 'candyBarData',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
