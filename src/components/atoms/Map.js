cc3992cfd2e153fa2cfb0877f99519bf749e506c
matt.giertl <matus1giertl@gmail.com>
2019-09-05 13:55:43 +0200

    map update

--- src/components/atoms/Map.js
+++ src/components/atoms/Map.js
@@ -17,7 +17,7 @@ const StyledMap = styled.iframe({
 const Map = () => (
   <StyledMap
     frameBorder="0"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2632.430861681838!2d19.139844751638496!3d48.716358279172105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47153ddfaf3eaaab%3A0xb4df25d8de57ab04!2sZvolensk%C3%A1+cesta+85%2C+974+05+Bansk%C3%A1+Bystrica%2C+Slovakia!5e0!3m2!1sen!2scz!4v1565868552093!5m2!1sen!2scz"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2632.43086167652!2d19.139850216066684!3d48.716358279273734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47153d53b480f2bf%3A0xf405cc77a8759513!2sMAKRONKOVO%20-%20Cukr%C3%A1rska%20manufakt%C3%BAra!5e0!3m2!1sen!2scz!4v1567684490826!5m2!1sen!2scz"
   />
 );
 
