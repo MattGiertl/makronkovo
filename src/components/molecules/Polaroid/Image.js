75caa410f923c2bf1c7a305c0cf15be9236e07ff
Rzavský Lukáš <rzavsky@rzavskymac.domain.local>
2019-09-23 13:58:23 +0200

    polaroid image - margin bottom added

--- src/components/molecules/Polaroid/Image.js
+++ src/components/molecules/Polaroid/Image.js
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled';
+import { mobileQuery } from '../../../utils/mediaqueries';
 
 const Image = styled.img({
   width: '100%',
   height: '70%',
   minHeight: '70%',
+  [mobileQuery]: {
+    minHeight: '65%',
+    marginBottom: '5px'
+  }
 });
 
 export default Image;
