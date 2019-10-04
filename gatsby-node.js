1112b3adf18cfb6fa69fe4e0a4c2df27d5688018
Rzavský Lukáš <rzavsky@rzavskymac.local>
2019-10-04 11:37:37 +0200

    nielen

--- gatsby-node.js
+++ gatsby-node.js
@@ -142,7 +142,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       header: 'CANDY BAR',
       upperText: 'Stiahnite si náš cennik pre candy bar:',
       lowerText:
-        'Dezerty Vám radi dovezieme chladiarenským autom a naaranžujeme v štýle Vašej svadby/akcie. Ohúrte Vašich hostí nie len skvelou chuťou ale aj vizuálnym zážitkom. Ceny dostupné na vyžiadanie.',
+        'Dezerty Vám radi dovezieme chladiarenským autom a naaranžujeme v štýle Vašej svadby/akcie. Ohúrte Vašich hostí nielen skvelou chuťou ale aj vizuálnym zážitkom. Ceny dostupné na vyžiadanie.',
       link: 'https://drive.google.com/file/d/18UpR-87uXENjDBIz5CXZpnB17aD0dMQb/view?usp=drivesdk',
     },
   });
